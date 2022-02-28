6d94823ed82f865eb08601efd82bf5675b81295e
fstipo <fstipanov@gmail.com>
2022-02-28 15:33:52 +0100

    Second version

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
 'use strict';
 
-document.addEventListener('keydown', (e) => {
-  const key = e.key;
+window.addEventListener('keydown', (e) => {
+  const key = e.key === ' ' ? 'Space' : e.key;
   const code = e.code;
   const keyCode = e.keyCode;
-  const btn = document.querySelector('button');
-  const output = document.querySelector('.output');
-  btn.style.display = 'none';
-  output.style.display = 'flex';
-  const boxes = document.querySelectorAll('.box');
-
-  for (let i = 0; i < boxes.length; i++) {
-    boxes[0].innerText = e.key;
-    boxes[1].innerText = e.code;
-    boxes[2].innerText = e.keyCode;
-  }
-
-  console.log(e.key);
-  console.log(e.code);
-  console.log(e.keyCode);
+  const container = document.querySelector('.container');
+  container.innerHTML = `<div class="key">${key}<small>event.key</small></div>
+  <div class="key">${code}<small>event.code</small></div>
+  <div class="key">${keyCode}<small>event.keyCode</small></div>`;
 });
